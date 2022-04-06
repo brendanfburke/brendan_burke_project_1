388c4d5ac8e48193846d30643ad9540f60b39cd9
brendanburke <bfburke33@gmail.com>
2022-04-06 14:38:01 -0400

    added some dom elements to the script, created arrays for board tiles and their values

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
-console.log('ss')
+const board = document.querySelector('.board-container')
+const boardArray = board.querySelectorAll('.tile')
+const values = [
+    0,0,4,0,
+    0,2,0,0,
+    0,0,0,0,
+    0,0,0,0
+]
+
+function valuePusher() {
+    for (let i = 0; i<values.length; i++) {
+        boardArray[i].innerHTML=values[i].toString()
+    }
+}
+
+valuePusher()
+console.log(boardArray)
+console.log(values)
