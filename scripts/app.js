2affcea29e0388c7d52ae779e845fa5965ab68a9
brendanburke <bfburke33@gmail.com>
2022-04-06 15:33:56 -0400

    added event listeners for arrow keys

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const board = document.querySelector('.board-container')
 const boardArray = board.querySelectorAll('.tile')
 const values = [
-    0,0,0,0,
+    2,2,0,0,
     0,0,0,0,
     0,0,0,0,
     0,0,0,0
@@ -13,6 +13,34 @@ function valuePusher() {
     }
 }
 
+function moveRight(e) {
+    if (e.keyCode === 39) {
+        console.log('right')
+    }
+}
+function moveLeft(e) {
+    if (e.keyCode === 37) {
+        console.log('left')
+    }
+}
+function moveUp(e) {
+    if (e.keyCode === 38) {
+        console.log('up')
+    }
+}
+function moveDown(e) {
+    if (e.keyCode === 40) {
+        console.log('down')
+    }
+}
+
+
 valuePusher()
 console.log(boardArray)
 console.log(values)
+
+
+document.addEventListener('keydown', moveRight)
+document.addEventListener('keydown', moveLeft)
+document.addEventListener('keydown', moveUp)
+document.addEventListener('keydown', moveDown)
