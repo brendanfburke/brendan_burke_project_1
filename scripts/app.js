ce8953781f70681cca39e556299f5114928e6f85
brendanburke <bfburke33@gmail.com>
2022-04-07 11:27:52 -0400

    added function to slide all tiles to the right if possible

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const board = document.querySelector('.board-container')
 const boardArray = board.querySelectorAll('.tile')
 const values = [
-    2,2,0,0,
+    0,0,0,0,
     0,0,0,0,
     0,0,0,0,
     0,0,0,0
@@ -17,9 +17,7 @@ function valuePusher() {
     }
 }
 
-const zeroCheck = (element) => element === 0;
 
-// console.log(values.some(zeroCheck))
 
 function checkValue(tile) {
     if (tile === 0) {
@@ -33,13 +31,11 @@ let newPopulation = 0
 
 function newNum() {
     let randomIndex = Math.random()
-    console.log(randomIndex)
     if (randomIndex < .8) {
         newPopulation = 2
     } else {
         newPopulation = 4
     }
-    console.log(newPopulation)
 }
 
 
@@ -48,18 +44,19 @@ function populateRandom() {
     let randomIndex = Math.floor(Math.random()*values.length)
     let square = checkValue(values[randomIndex])
     newNum()
-        if (square === true) {
-            values[randomIndex] = newPopulation
-            valuePusher()
-        } else if (square === false) {
-            populateRandom()
-        } else {
-            console.log('game over')
-        }
-    console.log(values.some(zeroCheck))
+    if (square === true) {
+        values[randomIndex] = newPopulation
+        valuePusher()
+    } else if (square === false) {
+        populateRandom()
+    } else {
+        console.log('game over')
+    }
 }
 
-function boardCheck() {
+const zeroCheck = (element) => element === 0;
+
+function lossCheck() {
     if (values.some(zeroCheck) === true) {
         populateRandom()
     } else if (values.some(zeroCheck) === false) {
@@ -70,19 +67,47 @@ function boardCheck() {
 }
 
 
+function slide(arr) {
+    for (let j = 0; j<3; j++){
+        for (let i = 0; i<3; i++) {
+            if (arr[i] > 0 && arr[i+1] === 0) {
+                arr[i+1] = arr[i]
+                arr[i] = 0
+            }
+        }
+        for (let i = 4; i<7; i++) {
+            if (arr[i] > 0 && arr[i+1] === 0) {
+                arr[i+1] = arr[i]
+                arr[i] = 0
+            }
+        }
+        for (let i = 8; i<11; i++) {
+            if (arr[i] > 0 && arr[i+1] === 0) {
+                arr[i+1] = arr[i]
+                arr[i] = 0
+            }
+        }
+        for (let i = 12; i<15; i++) {
+            if (arr[i] > 0 && arr[i+1] === 0) {
+                arr[i+1] = arr[i]
+                arr[i] = 0
+            }
+        }
+    }
+}
+
+
 function moveRight(e) {
-    let sum = values[0] + values[1]
     if (e.keyCode === 39) {
         console.log('right')
-        values[3] = sum
-        values[0] = 0
-        values[1] = 0
+        slide(values)
         valuePusher()
     }
 }
 function moveLeft(e) {
     if (e.keyCode === 37) {
-        boardCheck()
+        console.log('left')
+        lossCheck()
     }
 }
 function moveUp(e) {
