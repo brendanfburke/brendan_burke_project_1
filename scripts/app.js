c2d90cc7233398e76d91c925c51d10e728760401
brendanburke <bfburke33@gmail.com>
2022-04-08 14:33:55 -0400

    rewrote function to slide tiles, its now a universal function that works in all directions

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const board = document.querySelector('.board-container')
 const boardArray = board.querySelectorAll('.tile')
 const values = [
-    2,0,0,4,
+    0,0,0,0,
     0,0,0,0,
     0,0,0,0,
     0,0,0,0
@@ -19,8 +19,6 @@ function valuePusher() {
     }
 }
 
-
-
 function checkValue(tile) {
     if (tile === 0) {
         return true
@@ -33,14 +31,12 @@ let newPopulation = 0
 
 function newNum() {
     let randomIndex = Math.random()
-    if (randomIndex < .8) {
+    if (randomIndex < .9) {
         newPopulation = 2
     } else {
         newPopulation = 4
     }
 }
-
-
 
 function populateRandom() {
     let randomIndex = Math.floor(Math.random()*values.length)
@@ -68,17 +64,17 @@ function lossCheck() {
     }
 }
 
-
 lossCheck()
 lossCheck()
 
-function adderRight(arr, a, b, c, d) {
+
+function adder(arr, a, b, c, d) {
     if (arr[d] === arr[c]) {
         arr[d] *= 2
         arr[c] = 0
     } else if (arr[c] === 0 && arr[d] === arr[b]) {
         arr[d] *=2
-        arr[c] = 0
+        arr[b] = 0
     } else if (arr[c] === 0 && arr[b] === 0 && arr[d] === arr[a]) {
         arr[d] *=2
         arr[a] = 0
@@ -97,39 +93,27 @@ function adderRight(arr, a, b, c, d) {
 
 }
 
-function slideRight(arr) {
-    adderRight(values, 0, 1, 2, 3)
-    adderRight(values, 4, 5, 6, 7)
-    adderRight(values, 8, 9, 10, 11)
-    adderRight(values, 12, 13, 14, 15)
+
+
+function slideRight(arr, a, b, c, d) {
     
-    for (let j = 0; j<3; j++){
-        for (let i = 0; i<3; i++) {
-            if (arr[i] > 0 && arr[i+1] === 0) {
-                arr[i+1] = arr[i]
-                arr[i] = 0
-            }
-        }
-        for (let i = 4; i<7; i++) {
-            if (arr[i] > 0 && arr[i+1] === 0) {
-                arr[i+1] = arr[i]
-                arr[i] = 0
-            }
-        }
-        for (let i = 8; i<11; i++) {
-            if (arr[i] > 0 && arr[i+1] === 0) {
-                arr[i+1] = arr[i]
-                arr[i] = 0
-            }
-        }
-        for (let i = 12; i<15; i++) {
-            if (arr[i] > 0 && arr[i+1] === 0) {
-                arr[i+1] = arr[i]
-                arr[i] = 0
-            }
-        }
+
+    for (let i = 0; i<8; i++) {
+     if (arr[a] > 0 && arr[b] === 0) {
+         arr[b] = arr[a]
+         arr[a] = 0
+     } 
+     if (arr[b] > 0 && arr[c] === 0) {
+         arr[c] = arr[b]
+         arr[b] = 0
+     }
+     if (arr[c] > 0 && arr[d] === 0) {
+         arr[d] = arr[c]
+         arr[c] = 0
+     }
     }
-    lossCheck()
+    
+    
 }
 
 
@@ -137,23 +121,62 @@ function slideRight(arr) {
 function moveRight(e) {
     if (e.keyCode === 39) {
         console.log('right')
-        slideRight(values)
+        adder(values, 0, 1, 2, 3)
+        adder(values, 4, 5, 6, 7)
+        adder(values, 8, 9, 10, 11)
+        adder(values, 12, 13, 14, 15)
+        slideRight(values, 0, 1, 2, 3)
+        slideRight(values, 4, 5, 6, 7)
+        slideRight(values, 8, 9, 10, 11)
+        slideRight(values, 12, 13, 14, 15)
+        
+        lossCheck()
         valuePusher()
     }
 }
 function moveLeft(e) {
     if (e.keyCode === 37) {
         console.log('left')
+        adder(values, 3, 2, 1, 0)
+        adder(values, 7, 6, 5, 4)
+        adder(values, 11, 10, 9, 8)
+        adder(values, 15, 14, 13, 12)
+        slideRight(values, 3, 2, 1, 0)
+        slideRight(values, 7, 6, 5, 4)
+        slideRight(values, 11, 10, 9, 8)
+        slideRight(values, 15, 14, 13, 12)
+        lossCheck()
+        valuePusher()
     }
 }
 function moveUp(e) {
     if (e.keyCode === 38) {
         console.log('up')
+        adder(values, 15, 11, 7, 3)
+        adder(values, 14, 10, 6, 2)
+        adder(values, 13, 9, 5, 1)
+        adder(values, 12, 8, 4, 0)
+        slideRight(values, 15, 11, 7, 3)
+        slideRight(values, 14, 10, 6, 2)
+        slideRight(values, 13, 9, 5, 1)
+        slideRight(values, 12, 8, 4, 0)
+        lossCheck()
+        valuePusher()
     }
 }
 function moveDown(e) {
     if (e.keyCode === 40) {
         console.log('down')
+        adder(values, 3, 7, 11, 15)
+        adder(values, 2, 6, 10, 14)
+        adder(values, 1, 5, 9, 13)
+        adder(values, 0, 4, 8, 12)
+        slideRight(values, 3, 7, 11, 15)
+        slideRight(values, 2, 6, 10, 14)
+        slideRight(values, 1, 5, 9, 13)
+        slideRight(values, 0, 4, 8, 12)
+        lossCheck()
+        valuePusher()
     }
 }
 
